017c9f5cc9182255d97cd7bd48123150591ed114
James Titcumb <james@asgrim.com>
2013-10-18 09:07:49 +0100

    Added UA lookup tool

--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
 
 $baseHost = 'http://' . $_SERVER['SERVER_NAME'];
 
--- views/header.php
+++ views/header.php
@@ -10,6 +10,11 @@
 		<p id="textLogo">Browser Capabilities Project</p>
 		<p id="textLogoSubtitle">Providing regularly updated browscap.ini and related downloads since 1998!</p>
 
+		<ul>
+			<li><a href="/">Downloads</a></li>
+			<li><a href="/ua-lookup.php">UA Lookup</a></li>
+		</ul>
+
 		<div class="clear"></div>
 	</header>
 	<div id="body">
