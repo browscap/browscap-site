97650cfc2d8ec294c2115e953c40a9a2edcc82f1
James Titcumb <james@asgrim.com>
2017-12-05 16:21:39 -0800

    DB url must be parsed in ClearDB

--- config/autoload/heroku.global.php
+++ config/autoload/heroku.global.php
@@ -5,11 +5,15 @@ if (!getenv('HEROKU')) {
     return [];
 }
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 return [
     'debug' => getenv('DEBUG') === '1',
     \Zend\ConfigAggregator\ConfigAggregator::ENABLE_CACHE => true,
     'db' => [
-       'dsn' => getenv('CLEARDB_DATABASE_URL'),
+        'dsn' => sprintf('mysql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1)),
+        'user' => $url['user'],
+        'pass' => $url['pass'],
     ],
     'rateLimiter' => [
         'rateLimitDownloads' => 30, // How many downloads per $rateLimitPeriod
