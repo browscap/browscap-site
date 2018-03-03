a08ae8c595cfa54e802af64077a9746d48e5a05c
James Titcumb <james@asgrim.com>
2018-03-03 11:06:22 +0000

    Extracted site update logic

--- src/BrowscapSite/Tool/ComposerHook.php
+++ src/BrowscapSite/Tool/ComposerHook.php
@@ -3,18 +3,7 @@ declare(strict_types=1);
 
 namespace BrowscapSite\Tool;
 
-use Assert\Assert;
-use Browscap\Data\Factory\DataCollectionFactory;
-use Browscap\Parser\IniParser;
 use Composer\Script\Event;
-use Composer\IO\IOInterface;
-use Browscap\Generator\BuildGenerator;
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\ErrorLogHandler;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use Browscap\Writer\Factory\FullCollectionFactory;
-use PackageVersions\Versions;
 
 final class ComposerHook
 {
@@ -46,122 +35,6 @@ final class ComposerHook
      */
     public static function postUpdate(Event $event): void
     {
-        $buildNumber = self::determineBuildNumberFromPackage('browscap/browscap');
-
-        $currentBuildNumber = self::getCurrentBuildNumber();
-        if ($buildNumber !== $currentBuildNumber) {
-            $event->getIO()->write(sprintf('<info>Generating new Browscap build: %s</info>', $buildNumber));
-            self::createBuild($buildNumber, $event->getIO());
-            $event->getIO()->write('<info>All done</info>');
-        } else {
-            $event->getIO()->write(sprintf('<info>Current build %s is up to date</info>', $currentBuildNumber));
-        }
-    }
-
-    /**
-     * Converts a package number e.g. 1.2.3 into a "build number" e.g. 1002003
-     *
-     * There are three digits for each version, so 001002003 becomes 1002003 when cast to int to drop the leading zeros
-     *
-     * @param string $version
-     * @return int
-     */
-    private static function convertPackageVersionToBuildNumber(string $version): int
-    {
-        Assert::that($version)->regex('#^(\d+\.)(\d+\.)(\d+)$#');
-        return (int)sprintf('%03d%03d%03d', ...explode('.', $version));
-    }
-
-    /**
-     * Try to determine the build number from a composer package.
-     *
-     * @param string $packageName
-     * @return int
-     * @throws \OutOfBoundsException
-     */
-    private static function determineBuildNumberFromPackage(string $packageName): int
-    {
-        $packageVersion = Versions::getVersion($packageName);
-        return self::convertPackageVersionToBuildNumber(substr($packageVersion, 0, strpos($packageVersion, '@')));
-    }
-
-    /**
-     * @return int|null
-     */
-    private static function getCurrentBuildNumber(): ?int
-    {
-        $metadataFile = self::BUILD_DIRECTORY . '/metadata.php';
-
-        if (!file_exists($metadataFile)) {
-            return null;
-        }
-
-        /** @noinspection PhpIncludeInspection */
-        $metadata = require $metadataFile;
-        return (int)$metadata['version'];
-    }
-
-    /**
-     * Write a log message, if IO interface provided
-     *
-     * @param string $message
-     * @param IOInterface|null $io
-     */
-    private static function log(string $message, IOInterface $io = null): void
-    {
-        if ($io) {
-            $io->write($message);
-        }
-    }
-
-    /**
-     * Generate a build for build number specified.
-     *
-     * @param int $buildNumber
-     * @param IOInterface|null $io
-     * @throws \InvalidArgumentException
-     * @throws \RuntimeException
-     * @throws \BrowscapPHP\Exception
-     * @throws \Exception
-     * @throws \Assert\AssertionFailedException
-     */
-    private static function createBuild(int $buildNumber, IOInterface $io = null): void
-    {
-        if (!file_exists(self::BUILD_DIRECTORY)
-            && !mkdir(self::BUILD_DIRECTORY, 0775, true)
-            && !is_dir(self::BUILD_DIRECTORY)
-        ) {
-            throw new \RuntimeException(sprintf('Directory "%s" was not created', self::BUILD_DIRECTORY));
-        }
-
-        $logLevel = getenv('BC_BUILD_LOG') ?: Logger::NOTICE;
-
-        $stream = new StreamHandler('php://output', $logLevel);
-        $stream->setFormatter(new LineFormatter('%message%' . "\n"));
-
-        $logger = new Logger('browscap');
-        $logger->pushHandler($stream);
-        $logger->pushHandler(new ErrorLogHandler(ErrorLogHandler::OPERATING_SYSTEM, $logLevel));
-
-        self::log('  - Creating browscap build', $io);
-        $buildGenerator = new BuildGenerator(
-            self::RESOURCE_DIRECTORY,
-            self::BUILD_DIRECTORY,
-            $logger,
-            (new FullCollectionFactory())->createCollection($logger, self::BUILD_DIRECTORY),
-            new DataCollectionFactory($logger)
-        );
-        $buildGenerator->run((string)$buildNumber);
-
-        // Generate the metadata for the site
-        self::log('  - Generating metadata', $io);
-        (new RebuildMetadata(
-            new IniParser(self::BUILD_DIRECTORY . '/browscap.ini'),
-            self::BUILD_DIRECTORY
-        ))->rebuildMetadata();
-
-        // Updating browscap.ini cache
-        self::log('  - Updating cache...');
-        (new BrowscapPhpTool())->update();
+        (new UpdateBrowscapVersionUsed(self::BUILD_DIRECTORY, self::RESOURCE_DIRECTORY))->__invoke($event->getIO());
     }
 }
---
+++ src/BrowscapSite/Tool/UpdateBrowscapVersionUsed.php
@@ -0,0 +1,150 @@
+<?php
+declare(strict_types=1);
+
+namespace BrowscapSite\Tool;
+
+use Assert\Assert;
+use Browscap\Data\Factory\DataCollectionFactory;
+use Browscap\Generator\BuildGenerator;
+use Browscap\Parser\IniParser;
+use Browscap\Writer\Factory\FullCollectionFactory;
+use Composer\IO\IOInterface;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\ErrorLogHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use PackageVersions\Versions;
+
+final class UpdateBrowscapVersionUsed
+{
+    /**
+     * @var string
+     */
+    private $buildDirectory;
+
+    /**
+     * @var string
+     */
+    private $resourceDirectory;
+
+    public function __construct(string $buildDirectory, string $resourceDirectory)
+    {
+        $this->buildDirectory = $buildDirectory;
+        $this->resourceDirectory = $resourceDirectory;
+    }
+
+    /**
+     * @param IOInterface $io
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     * @throws \OutOfBoundsException
+     * @throws \Assert\AssertionFailedException
+     * @throws \BrowscapPHP\Exception
+     * @throws \Exception
+     */
+    public function __invoke(IOInterface $io): void
+    {
+        $packageBuildNumber = $this->determineBuildNumberFromPackage('browscap/browscap');
+        $currentBuildNumber = $this->getCurrentBuildNumber();
+
+        if ($packageBuildNumber !== $currentBuildNumber) {
+            $io->write(sprintf('<info>Generating new Browscap build: %s</info>', $packageBuildNumber));
+            $this->createBuild($packageBuildNumber, $io);
+            $io->write('<info>All done</info>');
+        } else {
+            $io->write(sprintf('<info>Current build %s is up to date</info>', $currentBuildNumber));
+        }
+    }
+
+    /**
+     * Converts a package number e.g. 1.2.3 into a "build number" e.g. 1002003
+     *
+     * There are three digits for each version, so 001002003 becomes 1002003 when cast to int to drop the leading zeros
+     *
+     * @param string $version
+     * @return int
+     */
+    private function convertPackageVersionToBuildNumber(string $version): int
+    {
+        Assert::that($version)->regex('#^(\d+\.)(\d+\.)(\d+)$#');
+        return (int)sprintf('%03d%03d%03d', ...explode('.', $version));
+    }
+
+    /**
+     * Try to determine the build number from a composer package.
+     *
+     * @param string $packageName
+     * @return int
+     * @throws \OutOfBoundsException
+     */
+    private function determineBuildNumberFromPackage(string $packageName): int
+    {
+        $packageVersion = Versions::getVersion($packageName);
+        return $this->convertPackageVersionToBuildNumber(substr($packageVersion, 0, strpos($packageVersion, '@')));
+    }
+
+    /**
+     * @return int|null
+     */
+    private function getCurrentBuildNumber(): ?int
+    {
+        $metadataFile = $this->buildDirectory . '/metadata.php';
+
+        if (!file_exists($metadataFile)) {
+            return null;
+        }
+
+        /** @noinspection PhpIncludeInspection */
+        $metadata = require $metadataFile;
+        return (int)$metadata['version'];
+    }
+
+    /**
+     * Generate a build for build number specified.
+     *
+     * @param int $buildNumber
+     * @param IOInterface|null $io
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
+     * @throws \BrowscapPHP\Exception
+     * @throws \Exception
+     * @throws \Assert\AssertionFailedException
+     */
+    private function createBuild(int $buildNumber, IOInterface $io): void
+    {
+        if (!file_exists($this->buildDirectory)
+            && !mkdir($this->buildDirectory, 0775, true)
+            && !is_dir($this->buildDirectory)
+        ) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $this->buildDirectory));
+        }
+
+        $logLevel = getenv('BC_BUILD_LOG') ?: Logger::NOTICE;
+
+        $stream = new StreamHandler('php://output', $logLevel);
+        $stream->setFormatter(new LineFormatter('%message%' . "\n"));
+
+        $logger = new Logger('browscap');
+        $logger->pushHandler($stream);
+        $logger->pushHandler(new ErrorLogHandler(ErrorLogHandler::OPERATING_SYSTEM, $logLevel));
+
+        $io->write('  - Creating browscap build');
+        $buildGenerator = new BuildGenerator(
+            $this->resourceDirectory,
+            $this->buildDirectory,
+            $logger,
+            (new FullCollectionFactory())->createCollection($logger, $this->buildDirectory),
+            new DataCollectionFactory($logger)
+        );
+        $buildGenerator->run((string)$buildNumber);
+
+        $io->write('  - Generating metadata');
+        (new RebuildMetadata(
+            new IniParser($this->buildDirectory . '/browscap.ini'),
+            $this->buildDirectory
+        ))->rebuildMetadata();
+
+        $io->write('  - Updating cache');
+        (new BrowscapPhpTool())->update();
+    }
+}
