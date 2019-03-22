e451be02112f2471bd68b5b823f078f5b9739f1f
James Titcumb <james@asgrim.com>
2019-03-22 13:00:19 +0000

    Don't use config caching for Heroku for now as closures being used in container

--- config/autoload/heroku.global.php
+++ config/autoload/heroku.global.php
@@ -9,7 +9,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 return [
     'debug' => getenv('DEBUG') === '1',
-    \Zend\ConfigAggregator\ConfigAggregator::ENABLE_CACHE => true,
+    \Zend\ConfigAggregator\ConfigAggregator::ENABLE_CACHE => false,
     'db' => [
         'dsn' => sprintf('mysql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1)),
         'user' => $url['user'],
