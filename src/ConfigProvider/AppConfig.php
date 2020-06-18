c4d2d98bef504872b8f35cd3fed9eae1d28cf9f9
James Titcumb <james@asgrim.com>
2020-06-18 11:33:54 +0100

    Added some happy-path unit tests for some critical parts

--- src/ConfigProvider/AppConfig.php
+++ src/ConfigProvider/AppConfig.php
@@ -19,6 +19,7 @@ use BrowscapSite\Renderer\Renderer;
 use BrowscapSite\Renderer\TwigRenderer;
 use BrowscapSite\Tool\AnalyseStatistics;
 use BrowscapSite\Tool\DeleteOldDownloadLogs;
+use BrowscapSite\Tool\PdoRateLimiter;
 use BrowscapSite\Tool\RateLimiter;
 use BrowscapSite\UserAgentTool\BrowscapPhpUserAgentTool;
 use BrowscapSite\UserAgentTool\UserAgentTool;
@@ -69,6 +70,72 @@ use function getenv;
  */
 final class AppConfig
 {
+    public const DEFAULT_BAN_CONFIGURATION = [
+        'rateLimitDownloads' => 30, // How many downloads per $rateLimitPeriod
+        'rateLimitPeriod' => 1, // Download limit period in HOURS
+        'tempBanPeriod' => 3, // Tempban period in DAYS
+        'tempBanLimit' => 5, // How many tempbans allowed in $tempBanPeriod before permaban
+    ];
+
+    public const DEFAULT_FILES_LIST = [
+        'asp' => [
+            'BrowsCapINI' => [
+                'name' => 'browscap.ini',
+                'size' => null,
+                'description' => 'This is the standard version of browscap.ini file for IIS 5.x and greater.',
+            ],
+            'Full_BrowsCapINI' => [
+                'name' => 'full_asp_browscap.ini',
+                'size' => null,
+                'description' => 'This is a larger version of browscap.ini with all the new properties.',
+            ],
+            'Lite_BrowsCapINI' => [
+                'name' => 'lite_asp_browscap.ini',
+                'size' => null,
+                'description' => 'This is a smaller version of browscap.ini file containing major browsers & search engines. This file is adequate for most websites.',
+            ],
+        ],
+        'php' => [
+            'PHP_BrowsCapINI' => [
+                'name' => 'php_browscap.ini',
+                'size' => null,
+                'description' => 'This is a special version of browscap.ini for PHP users only!',
+            ],
+            'Full_PHP_BrowsCapINI' => [
+                'name' => 'full_php_browscap.ini',
+                'size' => null,
+                'description' => 'This is a larger version of php_browscap.ini with all the new properties.',
+            ],
+            'Lite_PHP_BrowsCapINI' => [
+                'name' => 'lite_php_browscap.ini',
+                'size' => null,
+                'description' => 'This is a smaller version of php_browscap.ini file containing major browsers & search engines. This file is adequate for most websites.',
+            ],
+        ],
+        'other' => [
+            'BrowsCapXML' => [
+                'name' => 'browscap.xml',
+                'size' => null,
+                'description' => 'This is the standard version of browscap.ini file in XML format.',
+            ],
+            'BrowsCapCSV' => [
+                'name' => 'browscap.csv',
+                'size' => null,
+                'description' => 'This is an industry-standard comma-separated-values version of browscap.ini. Easily imported into Access, Excel, MySQL & others.',
+            ],
+            'BrowsCapJSON' => [
+                'name' => 'browscap.json',
+                'size' => null,
+                'description' => 'This is a JSON (JavaScript Object Notation) version of browscap.ini. This is usually used with JavaScript.',
+            ],
+            'BrowsCapZIP' => [
+                'name' => 'browscap.zip',
+                'size' => null,
+                'description' => 'This archive combines all the above files into one download that is smaller than all eight files put together.',
+            ],
+        ],
+    ];
+
     private const BAN_CONFIGURATION   = 'banConfiguration';
     private const BROWSCAP_FILES_LIST = 'browscapFilesList';
 
@@ -77,12 +144,7 @@ final class AppConfig
     {
         return [
             'dependencies' => $this->dependencies(),
-            'rateLimiter' => [
-                'rateLimitDownloads' => 30, // How many downloads per $rateLimitPeriod
-                'rateLimitPeriod' => 1, // Download limit period in HOURS
-                'tempBanPeriod' => 3, // Tempban period in DAYS
-                'tempBanLimit' => 5, // How many tempbans allowed in $tempBanPeriod before permaban
-            ],
+            'rateLimiter' => self::DEFAULT_BAN_CONFIGURATION,
             'db' => [
                 'dsn' => 'mysql:dbname=browscap',
                 'user' => '',
@@ -126,8 +188,11 @@ final class AppConfig
 
                     return $banConfiguration;
                 },
-                RateLimiter::class => static function (ContainerInterface $container): RateLimiter {
-                    return new RateLimiter($container->get(PDO::class), $container->get(self::BAN_CONFIGURATION));
+                RateLimiter::class => static function (ContainerInterface $container): PdoRateLimiter {
+                    return new PdoRateLimiter(
+                        $container->get(PDO::class),
+                        $container->get(self::BAN_CONFIGURATION)
+                    );
                 },
                 Metadata::class => static function (): Metadata {
                     return Metadata::fromArray(require __DIR__ . '/../../vendor/build/metadata.php');
@@ -194,64 +259,7 @@ final class AppConfig
                     return new DeleteOldDownloadLogs($container->get(PDO::class));
                 },
                 self::BROWSCAP_FILES_LIST => static function (): array {
-                    return [
-                        'asp' => [
-                            'BrowsCapINI' => [
-                                'name' => 'browscap.ini',
-                                'size' => null,
-                                'description' => 'This is the standard version of browscap.ini file for IIS 5.x and greater.',
-                            ],
-                            'Full_BrowsCapINI' => [
-                                'name' => 'full_asp_browscap.ini',
-                                'size' => null,
-                                'description' => 'This is a larger version of browscap.ini with all the new properties.',
-                            ],
-                            'Lite_BrowsCapINI' => [
-                                'name' => 'lite_asp_browscap.ini',
-                                'size' => null,
-                                'description' => 'This is a smaller version of browscap.ini file containing major browsers & search engines. This file is adequate for most websites.',
-                            ],
-                        ],
-                        'php' => [
-                            'PHP_BrowsCapINI' => [
-                                'name' => 'php_browscap.ini',
-                                'size' => null,
-                                'description' => 'This is a special version of browscap.ini for PHP users only!',
-                            ],
-                            'Full_PHP_BrowsCapINI' => [
-                                'name' => 'full_php_browscap.ini',
-                                'size' => null,
-                                'description' => 'This is a larger version of php_browscap.ini with all the new properties.',
-                            ],
-                            'Lite_PHP_BrowsCapINI' => [
-                                'name' => 'lite_php_browscap.ini',
-                                'size' => null,
-                                'description' => 'This is a smaller version of php_browscap.ini file containing major browsers & search engines. This file is adequate for most websites.',
-                            ],
-                        ],
-                        'other' => [
-                            'BrowsCapXML' => [
-                                'name' => 'browscap.xml',
-                                'size' => null,
-                                'description' => 'This is the standard version of browscap.ini file in XML format.',
-                            ],
-                            'BrowsCapCSV' => [
-                                'name' => 'browscap.csv',
-                                'size' => null,
-                                'description' => 'This is an industry-standard comma-separated-values version of browscap.ini. Easily imported into Access, Excel, MySQL & others.',
-                            ],
-                            'BrowsCapJSON' => [
-                                'name' => 'browscap.json',
-                                'size' => null,
-                                'description' => 'This is a JSON (JavaScript Object Notation) version of browscap.ini. This is usually used with JavaScript.',
-                            ],
-                            'BrowsCapZIP' => [
-                                'name' => 'browscap.zip',
-                                'size' => null,
-                                'description' => 'This archive combines all the above files into one download that is smaller than all eight files put together.',
-                            ],
-                        ],
-                    ];
+                    return self::DEFAULT_FILES_LIST;
                 },
             ],
         ];
---
+++ tests/Handler/DownloadHandlerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrowscapSiteTest\Handler;
+
+use BrowscapSite\ConfigProvider\AppConfig;
+use BrowscapSite\Handler\DownloadHandler;
+use BrowscapSite\Metadata\Metadata;
+use BrowscapSite\Renderer\Renderer;
+use BrowscapSiteTest\TestHelper;
+use DateTimeImmutable;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\ServerRequest;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/** @covers \BrowscapSite\Handler\DownloadHandler */
+final class DownloadHandlerTest extends TestCase
+{
+    /** @var Renderer&MockObject */
+    private $renderer;
+    private Metadata $metadata;
+    private DateTimeImmutable $releaseDate;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->releaseDate = new DateTimeImmutable();
+
+        $this->renderer = $this->createMock(Renderer::class);
+        $this->metadata = TestHelper::createMetadataForTesting(TestHelper::EXAMPLE_RELEASE_VERSION, $this->releaseDate);
+    }
+
+    public function testHandleRendersFileList(): void
+    {
+        $handler = new DownloadHandler(
+            $this->renderer,
+            $this->metadata,
+            AppConfig::DEFAULT_FILES_LIST,
+            AppConfig::DEFAULT_BAN_CONFIGURATION
+        );
+
+        $expectedResponse = new HtmlResponse('', 200);
+        $this->renderer
+            ->expects(self::once())
+            ->method('render')
+            ->with(
+                'downloads.html',
+                [
+                    'files' => [
+                        'asp' => [
+                            'BrowsCapINI' => [
+                                'name' => 'browscap.ini',
+                                'size' => '1,001',
+                                'description' => 'This is the standard version of browscap.ini file for IIS 5.x and greater.',
+                            ],
+                            'Full_BrowsCapINI' => [
+                                'name' => 'full_asp_browscap.ini',
+                                'size' => '1,002',
+                                'description' => 'This is a larger version of browscap.ini with all the new properties.',
+                            ],
+                            'Lite_BrowsCapINI' => [
+                                'name' => 'lite_asp_browscap.ini',
+                                'size' => '1,003',
+                                'description' => 'This is a smaller version of browscap.ini file containing major browsers & search engines. This file is adequate for most websites.',
+                            ],
+                        ],
+                        'php' => [
+                            'PHP_BrowsCapINI' => [
+                                'name' => 'php_browscap.ini',
+                                'size' => '1,004',
+                                'description' => 'This is a special version of browscap.ini for PHP users only!',
+                            ],
+                            'Full_PHP_BrowsCapINI' => [
+                                'name' => 'full_php_browscap.ini',
+                                'size' => '1,005',
+                                'description' => 'This is a larger version of php_browscap.ini with all the new properties.',
+                            ],
+                            'Lite_PHP_BrowsCapINI' => [
+                                'name' => 'lite_php_browscap.ini',
+                                'size' => '1,006',
+                                'description' => 'This is a smaller version of php_browscap.ini file containing major browsers & search engines. This file is adequate for most websites.',
+                            ],
+                        ],
+                        'other' => [
+                            'BrowsCapXML' => [
+                                'name' => 'browscap.xml',
+                                'size' => '1,007',
+                                'description' => 'This is the standard version of browscap.ini file in XML format.',
+                            ],
+                            'BrowsCapCSV' => [
+                                'name' => 'browscap.csv',
+                                'size' => '1,008',
+                                'description' => 'This is an industry-standard comma-separated-values version of browscap.ini. Easily imported into Access, Excel, MySQL & others.',
+                            ],
+                            'BrowsCapJSON' => [
+                                'name' => 'browscap.json',
+                                'size' => '1,009',
+                                'description' => 'This is a JSON (JavaScript Object Notation) version of browscap.ini. This is usually used with JavaScript.',
+                            ],
+                            'BrowsCapZIP' => [
+                                'name' => 'browscap.zip',
+                                'size' => '1,010',
+                                'description' => 'This archive combines all the above files into one download that is smaller than all eight files put together.',
+                            ],
+                        ],
+                    ],
+                    'version' => TestHelper::EXAMPLE_RELEASE_VERSION,
+                    'releaseDate' => $this->releaseDate->format('jS M Y'),
+                    'banConfig' => AppConfig::DEFAULT_BAN_CONFIGURATION,
+                ]
+            )
+            ->willReturn($expectedResponse);
+
+        self::assertSame($expectedResponse, $handler->handle(new ServerRequest()));
+    }
+}
